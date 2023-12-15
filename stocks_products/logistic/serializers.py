3d62905316ac45ad7b7c8cccefd0e374cc82120d
VV <wladivir1@mail.ru>
2023-12-15 11:18:55 +0500

    change serialisers

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -35,9 +35,7 @@ class StockSerializer(serializers.ModelSerializer):
         positions = validated_data.pop('positions')   
         # создаем склад по его параметрам
         stock = super().create(validated_data)
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        
         for position in positions:
             StockProduct.objects.create(stock=stock, **position)
         
@@ -48,9 +46,7 @@ class StockSerializer(serializers.ModelSerializer):
         positions = validated_data.pop('positions')
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+       
         for position in positions:
             StockProduct.objects.update_or_create(
                                                   stock=stock, 
